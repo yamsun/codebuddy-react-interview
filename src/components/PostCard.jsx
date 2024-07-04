84dccd78a704c7afdf17f83fa0fffb09c0988d80
Shyam Sundar <ssjha360@gmail.com>
2024-07-04 16:25:52 +0530

    posts - responsiveness

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -2,29 +2,26 @@ import PropTypes from "prop-types";
 
 const PostCard = ({ post }) => {
   return (
-    <div>
-      <div key={post?.id} className="post">
-        <div className="flex items-center gap-2 p-2">
+    <div className="post overflow-hidden rounded-lg bg-white shadow-lg">
+      <div key={post?.id}>
+        <div className="flex items-center gap-2 p-4">
           <img
             src={post.avatar}
             alt={`${post.firstName} ${post.lastName}`}
-            width={10}
-            height={10}
             className="h-10 w-10 rounded-full p-0.5 ring-2 ring-gray-300 dark:ring-gray-500"
           />
-          <h3 className="text-xl">
+          <h3 className="text-xl font-semibold">
             {post.firstName} {post.lastName}
           </h3>
         </div>
-        <div className="flex flex-col items-center gap-2 p-1">
+        <div className="flex flex-col items-center gap-2 p-4">
           <img
             src={post.image}
             alt={post.writeup}
-            className="h-auto w-full max-w-96 rounded-lg shadow-md dark:shadow-gray-800"
+            className="h-auto w-full rounded-lg shadow-sm dark:shadow-gray-800"
           />
           <p className="text-lg">{post.writeup}</p>
         </div>
-        <hr className="my-8 h-px border-0 bg-gray-200 dark:bg-gray-700" />
       </div>
     </div>
   );
